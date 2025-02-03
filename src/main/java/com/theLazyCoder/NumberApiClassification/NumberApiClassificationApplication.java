cbf5ceb1cd31cc2d5115f43ba561b9bd17f5a1aa
DevTom <72770851+Olatomiw@users.noreply.github.com>
2025-02-03 11:16:26 +0100

    Basic Setup

---
+++ src/main/java/com/theLazyCoder/NumberApiClassification/NumberApiClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.theLazyCoder.NumberApiClassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberApiClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberApiClassificationApplication.class, args);
+	}
+
+}
