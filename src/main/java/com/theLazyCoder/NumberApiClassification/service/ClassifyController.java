bfb82c29e8d066732b45823b2c9bb6cb24d65abc
DevTom <72770851+Olatomiw@users.noreply.github.com>
2025-02-07 22:34:07 +0100

    Final push

---
+++ src/main/java/com/theLazyCoder/NumberApiClassification/service/ClassifyController.java
@@ -0,0 +1,35 @@
+package com.theLazyCoder.NumberApiClassification.service;
+
+import com.theLazyCoder.NumberApiClassification.response.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.awt.*;
+
+@RestController("api")
+public class ClassifyController {
+
+    private final NumberClassificationService numberClassificationService;
+
+    public ClassifyController(NumberClassificationService numberClassificationService) {
+        this.numberClassificationService = numberClassificationService;
+    }
+
+    @PostMapping("/classify-number")
+    public ResponseEntity<?> classify(@RequestParam int number) {
+        return numberClassificationService.classifyNumber(number);
+    }
+
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Alphabet", true
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
