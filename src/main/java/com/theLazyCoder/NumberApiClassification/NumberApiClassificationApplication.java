192445420d128febea0c5571e8f3f4a05601131e
DevTom <72770851+Olatomiw@users.noreply.github.com>
2025-02-07 19:49:15 +0100

    done but readme

--- src/main/java/com/theLazyCoder/NumberApiClassification/NumberApiClassificationApplication.java
+++ src/main/java/com/theLazyCoder/NumberApiClassification/NumberApiClassificationApplication.java
@@ -1,13 +1,18 @@
 package com.theLazyCoder.NumberApiClassification;
 
+import com.theLazyCoder.NumberApiClassification.service.NumberClassificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static com.theLazyCoder.NumberApiClassification.service.NumberClassificationService.is_perfect_number;
 
 @SpringBootApplication
 public class NumberApiClassificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NumberApiClassificationApplication.class, args);
+		NumberClassificationService numberClassificationService = new NumberClassificationService();
+		numberClassificationService.classifyNumber(100);
+		is_perfect_number(28);
 	}
 
 }
